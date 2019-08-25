09384e8cbf396d48e0e4208b370d6bda9e72fdd9
AugustoCalado <augusto.bueno@usp.br>
2019-08-25 12:02:13 -0300

    Uploading Project

---
+++ src/main/kotlin/com/integrated/medical/recors/IntegratedMedicalRecordsApplication.kt
@@ -0,0 +1,11 @@
+package com.integrated.medical.recors
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntegratedMedicalRecordsApplication
+
+fun main(args: Array<String>) {
+	runApplication<IntegratedMedicalRecordsApplication>(*args)
+}

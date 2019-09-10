62450f89e7d0bf64cc31b69e04e4320e09fc912c
AugustoCalado <augusto.bueno@usp.br>
2019-09-09 21:10:23 -0300

    Added Spring Data Rest Annotation in PatientRepository

--- src/main/kotlin/com/integrated/medical/records/repository/PatientRepository.kt
+++ src/main/kotlin/com/integrated/medical/records/repository/PatientRepository.kt
@@ -4,9 +4,10 @@ import com.integrated.medical.records.domain.Patient
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
+//@RepositoryRestResource
 @Repository
 interface PatientRepository: CrudRepository<Patient,Int>{
-
+//    @RestResource(path = "names", rel = "name")
     fun findByName(name:String)
 
     fun findAllByBirthDateNotNull():List<Patient>

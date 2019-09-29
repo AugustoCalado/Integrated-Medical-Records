de1426a0bb34710c806e0f468523ab3db4fc4027
AugustoCalado <augusto.bueno@usp.br>
2019-09-29 13:56:32 -0300

    Added annotation @repository in HealthInsuranceRepository and ImunnizationScheduleRepository

--- src/main/kotlin/com/integrated/medical/records/repository/ImmunizationScheduleRepository.kt
+++ src/main/kotlin/com/integrated/medical/records/repository/ImmunizationScheduleRepository.kt
@@ -2,6 +2,8 @@ package com.integrated.medical.records.repository
 
 import com.integrated.medical.records.domain.ImmunizationSchedule
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ImmunizationScheduleRepository : CrudRepository<ImmunizationSchedule, Int> {
 }

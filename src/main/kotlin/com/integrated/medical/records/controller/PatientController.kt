7bd0facac233490678f1b26fa79c8aaf394f8ee1
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-10-07 21:42:46 -0300

    Inclusion of new methods in patientController [updatePatientInformation and getPatient]

--- src/main/kotlin/com/integrated/medical/records/controller/PatientController.kt
+++ src/main/kotlin/com/integrated/medical/records/controller/PatientController.kt
@@ -1,30 +1,47 @@
 package com.integrated.medical.records.controller
 
-import com.integrated.medical.records.domain.Patient
-import com.integrated.medical.records.repository.PatientRepository
+import com.integrated.medical.records.domain.dto.PatientDTO
+import com.integrated.medical.records.exception.PatientNotFoundException
+import com.integrated.medical.records.exception.PatientUpdateException
+import com.integrated.medical.records.service.PatientService
 import io.swagger.annotations.Api
 import org.springframework.data.rest.webmvc.RepositoryRestController
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
-@Api(value = "Immunization Patient Management System")
-//@RepositoryRestController
-@RestController
-@RequestMapping(path = ["/patient"])
-class PatientController {
+@Api(value = "Patient Management System")
+@RepositoryRestController
+class PatientController(
+        val patientService: PatientService
+) {
 
-    lateinit var patientRepository: PatientRepository
-
-    @GetMapping(path = ["/getYoungersPatients"])
-    // ResponseEntity<Resource<PatientResource>>
-    fun getPatientsByBirthDate(): ResponseEntity<List<Patient>> {
-        val patients: List<Patient> = patientRepository.findAllByBirthDateNotNull()
-        patients.forEach { p -> println(p) }
-        return ResponseEntity(patients, HttpStatus.OK)
-
+    @GetMapping("/patient/get/{cpf}", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getPatient(@PathVariable(name = "cpf", required = true) cpf: String): ResponseEntity<PatientDTO> {
+        return try {
+            val patient = patientService.findPatientByCpf(cpf)
+            ResponseEntity(patient, HttpStatus.OK)
+        } catch (ex: PatientNotFoundException) {
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
     }
+
+    @PostMapping("/patient/update-information/{cpf}")
+    fun updatePatientInformation(
+            @RequestBody(required = true) patient: PatientDTO,
+            @PathVariable(name = "cpf", required = true) cpf: String
+    ): ResponseEntity<String> {
+        return try {
+            patientService.updatePatient(patient, cpf)
+            ResponseEntity(HttpStatus.OK)
+        } catch (ex: PatientUpdateException) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
 
 }

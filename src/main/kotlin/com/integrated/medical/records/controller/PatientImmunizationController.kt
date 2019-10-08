e65351020b04be9e86ba74a5263fd48f903bd470
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-10-07 21:42:27 -0300

    New implementation of patient ImmunizationControleler

--- src/main/kotlin/com/integrated/medical/records/controller/PatientImmunizationController.kt
+++ src/main/kotlin/com/integrated/medical/records/controller/PatientImmunizationController.kt
@@ -1,90 +1,70 @@
 package com.integrated.medical.records.controller
 
 import com.integrated.medical.records.domain.dto.PatientVaccinesDTO
+import com.integrated.medical.records.domain.dto.VaccinesDTO
+import com.integrated.medical.records.exception.PatientImmunizationNotFoundException
+import com.integrated.medical.records.service.PatientImmunizationService
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
+import org.springframework.data.rest.webmvc.RepositoryRestController
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
-import java.io.Serializable
-import java.time.LocalDate
 
 @Api(value = "Immunization Patient Management System")
-@RestController
-//@RepositoryRestController
-@RequestMapping("/patient-immunizations")
-class PatientImmunizationController {
+@RepositoryRestController
+class PatientImmunizationController(
+        val patientImmunizationService: PatientImmunizationService
+) {
 
     //TODO Introduce some way to authorize and authenticate
     //TODO include Swagger
 
-    @GetMapping("/test")
-    fun testController(): ResponseEntity<String> {
-        return ResponseEntity.ok("{Teste}")
-    }
-
-
     @ApiOperation(value = "Get all vaccine of a patient given the ID")
-    @GetMapping("/{idPatient}", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping("/patient-immunization/all", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getAllPatientImmunizations(
-            @PathVariable(value = "idPatient", required = true) idPatient: Int
+            @RequestParam(name = "cpf", required = true) cpf: String
     ): ResponseEntity<List<PatientVaccinesDTO>> {
+        val listOfImmunizations: List<PatientVaccinesDTO> = patientImmunizationService.findAllPatientImmunization(cpf)
+        return ResponseEntity(listOfImmunizations, HttpStatus.OK)
 
-        var listOfImmunizations = ArrayList<PatientVaccinesDTO>()
-
-        return ResponseEntity(
-                listOfImmunizations,
-                HttpStatus.OK
-        )
     }
 
     @ApiOperation(value = "Get a specific vaccine of a patient")
-    @GetMapping("/{idPatient}", produces = [MediaType.APPLICATION_JSON_VALUE], params = ["idPatientVaccine", "idPatient"])
+    @GetMapping("/patient-immunization", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getPattientImmunization(
-            @PathVariable(value = "idPatient", required = true) idPatient: Int,
+            @RequestParam(name = "idPatient", required = true) idPatient: Int,
             @RequestParam(name = "idPatientVaccine", required = true) idPatientVaccines: Int
     ): ResponseEntity<PatientVaccinesDTO> {
-
+        patientImmunizationService.findPatientImmunization(idPatient, idPatientVaccines)
         return ResponseEntity(HttpStatus.OK)
     }
 
     @ApiOperation(value = "Register the patient immunization")
-    @PostMapping(value = "/register")
-    fun <T> registerPatientImmunization(@RequestBody register: Map<String, Serializable>): ResponseEntity<T> {
-
-        return ResponseEntity(HttpStatus.OK)
-    }
-
-    @ApiOperation(value = "Register the patient immunization")
-    @PostMapping(value = "/register", params = ["idPatient", "idVaccine", "data", "place"])
-    fun <T> registerPatientImmunization(
-            @RequestParam(name = "idPatient", required = true) idPatient: Int,
-            @RequestParam(name = "idVaccine", required = true) idVaccine: Int,
-            @RequestParam(name = "data", required = true) immunizationDate: LocalDate,
-            @RequestParam(name = "place") placeImmunization: String
-    ): ResponseEntity<T> {
-
-    //This endpoint is going to receive parameters to registry one patient vaccination
-
-
+    @PostMapping("/patient-immunization/add-immunization/{cpf}")
+    fun addPatientImmunization(
+            @RequestParam(name = "cpf", required = true) cpf: String,
+            @RequestBody(required = true) vaccinesDTO: VaccinesDTO
+    ): ResponseEntity<String> {
+        patientImmunizationService.insertPatientImmunization()
         return ResponseEntity(HttpStatus.OK)
     }
 
     @ApiOperation(value = "Delete patient vaccine")
-    @DeleteMapping(value = "/delete/}")
-    fun <T> deletePatientVaccine(
-            @PathVariable(value = "idPatient", required = true) idPatient: Int,
+    @DeleteMapping("/patient-immunization/delete}")
+    fun deletePatientVaccine(
+            @RequestParam(name = "idPatient", required = true) idPatient: Int,
             @RequestParam(name = "idPatientVaccine", required = true) idPatientVaccines: Int
 
-    ): ResponseEntity<T> {
-
-
-        return ResponseEntity(HttpStatus.OK)
+    ): ResponseEntity<String> {
+        return try {
+            patientImmunizationService.deletePattientImmunization()
+            ResponseEntity(HttpStatus.OK)
+        } catch (ex: PatientImmunizationNotFoundException) {
+            ResponseEntity(ex.message, HttpStatus.NOT_FOUND)
+        }
     }
-
-
 
 
 }

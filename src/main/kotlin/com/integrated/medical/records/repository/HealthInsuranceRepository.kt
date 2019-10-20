10a469538e7a9eca268952ed7f2eb82956324717
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-10-20 10:56:03 -0300

    Remove HealthInsurance domain  controllers and service layers

--- src/main/kotlin/com/integrated/medical/records/repository/HealthInsuranceRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.integrated.medical.records.repository
-
-import com.integrated.medical.records.domain.HealthInsurance
-import org.springframework.data.repository.CrudRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
-
-@RepositoryRestResource
-interface HealthInsuranceRepository : CrudRepository<HealthInsurance, Int> {
-}

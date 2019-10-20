5bd4b96b6c4b6926a785ebf2851430d11c9948a8
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-10-19 23:15:04 -0300

    Refactoring HISTORIC entities.... Starting from now all historic will be treated through enums

--- src/main/kotlin/com/integrated/medical/records/repository/LifeHistoricRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.integrated.medical.records.repository
-
-import com.integrated.medical.records.domain.LifeHistoric
-import org.springframework.data.repository.CrudRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
-
-@RepositoryRestResource
-interface LifeHistoricRepository : CrudRepository<LifeHistoric, Int>

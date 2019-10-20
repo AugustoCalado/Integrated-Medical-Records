26b7aae145cc5d5ed934d69eaeb192efd3d34c80
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-10-20 11:38:35 -0300

    Convert entity medical prescription to string field in medicalRecord

--- src/main/kotlin/com/integrated/medical/records/repository/MedicalPrescriptionRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.integrated.medical.records.repository
-
-import com.integrated.medical.records.domain.MedicalPrescription
-import org.springframework.data.repository.CrudRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
-
-@RepositoryRestResource
-interface MedicalPrescriptionRepository : CrudRepository<MedicalPrescription, Int> {
-}

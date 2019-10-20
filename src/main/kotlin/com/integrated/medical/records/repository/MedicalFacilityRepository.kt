fae0e4813eb93228b7ab6621b127b364e9b9aeba
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-10-19 22:45:19 -0300

    Refactor entities -> deleted DoctorCredencial Object and MedicalFacility

--- src/main/kotlin/com/integrated/medical/records/repository/MedicalFacilityRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.integrated.medical.records.repository
-
-import com.integrated.medical.records.domain.MedicalFacility
-import org.springframework.data.repository.CrudRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
-
-@RepositoryRestResource
-interface MedicalFacilityRepository : CrudRepository<MedicalFacility, Int>

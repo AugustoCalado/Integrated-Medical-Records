96652d48dd177112af49b590fe61a89a486db213
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-10-26 20:43:37 -0300

    Implementation of service layer for emergencyCard

---
+++ src/main/kotlin/com/integrated/medical/records/service/Impl/EmergencyCardServiceImpl.kt
@@ -0,0 +1,53 @@
+package com.integrated.medical.records.service.Impl
+
+import com.integrated.medical.records.domain.EmergencyCard
+import com.integrated.medical.records.domain.dto.EmergencyCardDTO
+import com.integrated.medical.records.domain.dto.PatientDTO
+import com.integrated.medical.records.domain.dto.toEntity
+import com.integrated.medical.records.domain.toDTO
+import com.integrated.medical.records.exception.ObjectNotFoundException
+import com.integrated.medical.records.repository.EmergencyCardRepository
+import com.integrated.medical.records.repository.PatientRepository
+import com.integrated.medical.records.service.EmergencyCardService
+
+import org.springframework.stereotype.Service
+
+@Service
+class EmergencyCardServiceImpl(
+        val emergencyCardRepository: EmergencyCardRepository,
+        val patientRepository: PatientRepository
+) : EmergencyCardService {
+
+    @Throws(ObjectNotFoundException::class)
+    override fun getEmergencyCardItens(patientCPF: String): List<EmergencyCardDTO> {
+
+        val patient = patientRepository.findByCpf(patientCPF)
+                ?: throw ObjectNotFoundException("Patient not found")
+        patient.emergencyCard?.let {
+            return EmergencyCard.entityListToDtoList(it)
+        }
+        return emptyList()
+    }
+
+    @Throws(ObjectNotFoundException::class)
+    override fun insertEmergencyCardItem(patientCPF: String, emergencyCardItem: EmergencyCardDTO): PatientDTO {
+
+        val patient = patientRepository.findByCpf(patientCPF)
+                ?: throw ObjectNotFoundException("Patient not found")
+        patient.emergencyCard?.let {
+            it.add(emergencyCardItem.toEntity())
+        }
+        return patientRepository.save(patient).toDTO()
+    }
+
+    @Throws(ObjectNotFoundException::class)
+    override fun updateEmergencyCardItem(emergencyCardItemDTO: EmergencyCardDTO): EmergencyCardDTO {
+
+        emergencyCardRepository.findById(emergencyCardItemDTO.idEmergencyCardDTO).orElseThrow {
+            throw ObjectNotFoundException("Emergency Card not found")
+        }
+        return emergencyCardRepository.save(emergencyCardItemDTO.toEntity()).toDTO()
+    }
+
+
+}

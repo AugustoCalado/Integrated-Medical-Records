d63c04d41d9639989984063017043695589ed7ba
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2019-11-13 23:10:49 -0300

    Adding annotation to allow cross origin requests

--- src/main/kotlin/com/integrated/medical/records/config/CorsConfig.kt
+++ src/main/kotlin/com/integrated/medical/records/config/CorsConfig.kt
@@ -1,14 +1,13 @@
 package com.integrated.medical.records.config
 
-import org.apache.catalina.filters.CorsFilter
+import
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.boot.web.servlet.FilterRegistrationBean
-
-
+import org.springframework.web.filter.CorsFilter
 
 
 @Configuration
